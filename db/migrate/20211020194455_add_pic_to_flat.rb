ee3fd10426855d5837d28d07f5f441f1be598029
katherine colina <kathymcolina@gmail.com>
2021-10-20 17:34:42 -0400

    compled CRUD and CSS

---
+++ db/migrate/20211020194455_add_pic_to_flat.rb
@@ -0,0 +1,5 @@
+class AddPicToFlat < ActiveRecord::Migration[6.1]
+  def change
+    add_column :flats, :picture_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroy"
+Flat.destroy_all
+
+puts "created"
+
+10.times do |number|
+   Flat.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    picture_url: "https://source.unsplash.com/1600x900/?flats",
+    description: Faker::Lorem.sentence(word_count: 20),
+    price_per_night: rand(20..200),
+    number_of_guests: rand(1..8)
+  )
+end
